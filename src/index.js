61f87a939c0d955ea07099d85fd25d0efe368a0c
manish ranjan <ranjanmanish95@gmail.com>
2021-09-29 03:38:51 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import react from 'react';
+import reactDOM from 'react-dom';
+/*
+function getButtonText(){
+    return 'Click Me!';
+}
+*/
+const App = ()=>{
+  const buttonText = 'Click Me!'; 
+  const style = {backgroundColor:'red',color:'white'};
+    return(
+        <div>
+         <label htmlFor="name" className='label'>enter e-mail</label>
+         <input id='name' type='text'/>
+         <button style={style}>
+             {buttonText}
+         </button>
+        </div>
+    )
+}
+reactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
